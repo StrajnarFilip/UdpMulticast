3526e827460da6a52130c452c90bd6dd543e0bb7
Filip Strajnar <filip.strajnar@gmail.com>
2023-06-01 01:53:17 +0200

    Hopefully working!

---
+++ Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var multicastAddress = IPAddress.Parse("224.1.2.3");
+var destination = new IPEndPoint(multicastAddress, 1000);
+var multicastOption = new MulticastOption(multicastAddress);
+System.Console.WriteLine($"{multicastOption.InterfaceIndex} {multicastOption.LocalAddress} {multicastOption.Group}");
+UdpClient senderUdp = new(2000);
+
+senderUdp.JoinMulticastGroup(multicastAddress);
+
+senderUdp.Send(new byte[] { 1, 2, 3 }, destination);
