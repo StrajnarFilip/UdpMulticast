365d277ac779226c43e9f1e7910ac48f5aaff3bc
Filip Strajnar <filip.strajnar@gmail.com>
2023-06-01 13:01:19 +0200

    Changed ports

--- Client/Program.cs
+++ Client/Program.cs
@@ -4,8 +4,11 @@ using System.Net.Sockets;
 const string CLIENT_ADDRESS = "0.0.0.0";
 const int CLIENT_PORT = 1500;
 
-var multicastAddress = IPAddress.Parse("224.1.2.3");
-var source = new IPEndPoint(multicastAddress, 2000);
+const string SOURCE_ADDRESS = "224.1.2.3";
+const int SOURCE_PORT = 2000;
+
+var multicastAddress = IPAddress.Parse(SOURCE_ADDRESS);
+var source = new IPEndPoint(multicastAddress, SOURCE_PORT);
 UdpClient receiverUdp = new(new IPEndPoint(IPAddress.Parse(CLIENT_ADDRESS), CLIENT_PORT));
 
 receiverUdp.JoinMulticastGroup(multicastAddress);
--- Server/Program.cs
+++ Server/Program.cs
@@ -4,8 +4,11 @@ using System.Net.Sockets;
 const string SERVER_ADDRESS = "0.0.0.0";
 const int SERVER_PORT = 2000;
 
-var multicastAddress = IPAddress.Parse("224.1.2.3");
-var destination = new IPEndPoint(multicastAddress, 1000);
+const string DESTINATION_ADDRESS = "224.1.2.3";
+const int DESTINATION_PORT = 1500;
+
+var multicastAddress = IPAddress.Parse(DESTINATION_ADDRESS);
+var destination = new IPEndPoint(multicastAddress, DESTINATION_PORT);
 var multicastOption = new MulticastOption(multicastAddress);
 System.Console.WriteLine($"{multicastOption.InterfaceIndex} {multicastOption.LocalAddress} {multicastOption.Group}");
 UdpClient senderUdp = new(new IPEndPoint(IPAddress.Parse(SERVER_ADDRESS), SERVER_PORT));
